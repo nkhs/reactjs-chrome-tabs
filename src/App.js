3dab3d56d20751b3ef5107632e1d7da67d69745d
unknown <nkhs@yandex.com>
2021-03-23 03:56:58 +0800

    fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './App.css';
-
+import backSVG from './tab.svg'
 class App extends Component {
 
   componentDidMount() {
@@ -64,7 +64,7 @@ class App extends Component {
               <div className="chrome-tab">
                 <div className="chrome-tab-dividers"></div>
                 <div className="chrome-tab-background">
-                  {/* <svg version="1.1" xmlns="http://www.w3.org/2000/svg"><defs><symbol id="chrome-tab-geometry-left" viewBox="0 0 214 36"><path d="M17 0h197v36H0v-2c4.5 0 9-3.5 9-8V8c0-4.5 3.5-8 8-8z"/></symbol><symbol id="chrome-tab-geometry-right" viewBox="0 0 214 36"><use xlink:href="#chrome-tab-geometry-left"/></symbol><clipPath id="crop"><rect className="mask" width="100%" height="100%" x="0"/></clipPath></defs><svg width="52%" height="100%"><use xlink:href="#chrome-tab-geometry-left" width="214" height="36" className="chrome-tab-geometry"/></svg><g transform="scale(-1, 1)"><svg width="52%" height="100%" x="-100%" y="0"><use xlink:href="#chrome-tab-geometry-right" width="214" height="36" className="chrome-tab-geometry"/></svg></g></svg> */}
+                  <img alt='backg' src={backSVG}/>
                 </div>
                 <div className="chrome-tab-content">
                   <div className="chrome-tab-favicon google-fav"></div>
@@ -73,10 +73,10 @@ class App extends Component {
                   <div className="chrome-tab-close"></div>
                 </div>
               </div>
-              <div className="chrome-tab active">
+              <div className="chrome-tab " active>
                 <div className="chrome-tab-dividers"></div>
                 <div className="chrome-tab-background">
-                  {/* <svg version="1.1" xmlns="http://www.w3.org/2000/svg"><defs><symbol id="chrome-tab-geometry-left" viewBox="0 0 214 36"><path d="M17 0h197v36H0v-2c4.5 0 9-3.5 9-8V8c0-4.5 3.5-8 8-8z"/></symbol><symbol id="chrome-tab-geometry-right" viewBox="0 0 214 36"><use xlink:href="#chrome-tab-geometry-left"/></symbol><clipPath id="crop"><rect className="mask" width="100%" height="100%" x="0"/></clipPath></defs><svg width="52%" height="100%"><use xlink:href="#chrome-tab-geometry-left" width="214" height="36" className="chrome-tab-geometry"/></svg><g transform="scale(-1, 1)"><svg width="52%" height="100%" x="-100%" y="0"><use xlink:href="#chrome-tab-geometry-right" width="214" height="36" className="chrome-tab-geometry"/></svg></g></svg> */}
+                  <svg version="1.1" xmlns="http://www.w3.org/2000/svg"><defs><symbol id="chrome-tab-geometry-left" viewBox="0 0 214 36"><path d="M17 0h197v36H0v-2c4.5 0 9-3.5 9-8V8c0-4.5 3.5-8 8-8z"/></symbol><symbol id="chrome-tab-geometry-right" viewBox="0 0 214 36"><use xlink:href="#chrome-tab-geometry-left"/></symbol><clipPath id="crop"><rect className="mask" width="100%" height="100%" x="0"/></clipPath></defs><svg width="52%" height="100%"><use xlink:href="#chrome-tab-geometry-left" width="214" height="36" className="chrome-tab-geometry"/></svg><g transform="scale(-1, 1)"><svg width="52%" height="100%" x="-100%" y="0"><use xlink:href="#chrome-tab-geometry-right" width="214" height="36" className="chrome-tab-geometry"/></svg></g></svg>
                 </div>
                 <div className="chrome-tab-content">
                   <div className="chrome-tab-favicon facebook-fav"></div>
